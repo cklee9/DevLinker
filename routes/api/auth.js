52e993da1e3a2280af127da72cf5fae1b05c5815
Steve <chenkailee9@gmail.com>
2021-03-06 14:28:12 -0800

    implement authentication for users

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const User = require("../../models/User");
 
 // @route GET api/auth
 // @desc Test route
 // @access Public
-router.get('/', (req, res)=>res.send('Auth route'));
+// adding auth as a protector middleware for this route
+router.get("/", auth, async (req, res) => {
+  try {
+    console.log("Usre is", User);
+    const user = await User.findById(req.user.id).select("-password");
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
